789150f3411f6b70cd56b62cd54543b1367957d5
Sforzemon <sforzemon@gmail.com>
2020-01-04 02:47:51 -0500

    Clock was not updating and rearranging functions to they look nicer to me.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,73 @@
 $( document ).ready(function() {
+// New way to set time...
+$(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
+var update = function () {
+  $(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
+};
+function setTime() {
+setInterval(update, 1000);
+}
+setTime()
+//Set up my time slots
+  var startHour = 9;
+  var endHour = 5;
+  function createPlanner() {
+    for (j = 9; j < 18; j++){
+      if (j >= 12){
+        var newRow = $("<div>", {id: j, "class": "row"});
+        var checkTime = j
+        // console.log(checkTime);
+        if ( j > 12){
+          var PM = j - 12;
+        }
+        else {
+          var PM = 12;
+        }
+        var newTimeCol = $("<div>", {"class": "timeCol col-2"});
+        var newTextCol = $("<div>", {id: PM + " PM", "class": "TextCol col-9"});
+        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
+        newTimeCol.text(PM + " PM");
+        // console.log(checkTime);
+
+      }
+      else {
+        var AM = [j]
+        var checkTime = j;
+        // console.log(checkTime);
+        var newRow = $("<div>", {id: j, "class": "row"});
+        var newTimeCol = $("<div>", {"class": "timeCol col-2"});
+        var newTextCol = $("<div>", {id: AM + " AM", "class": "TextCol col-9"});
+        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
+        newTimeCol.text(AM + " AM");
+        // console.log(checkTime)
+      }
+      var saveButton = $("<button>", {"class": "saveButton"});
+      $(".planner-body").append(newRow);
+      newRow.append(newTimeCol, newTextCol, newSaveCol);
+      var inputText = $('<textarea class="h-100 w-100" placeholder="So, what should I remember for you?">');
+      newTextCol.append(inputText);
+      newSaveCol.append(saveButton);
+      var HH = parseInt(moment().format('HH'), 10);
+      if (checkTime < HH) {
+        newTextCol.addClass("past");
+      }
+      else if (checkTime > HH) {
+        newTextCol.addClass("future");
+      }
+      else {
+        newTextCol.addClass("present");
+      }
+      // console.log(HH);
+      // console.log(checkTime);
+    }
+  }
+  createPlanner();
+});
+
+
+
+
+
 // Display the time in the jumbotron
 // Missed the whole use Moment thing... On the plus side, my way worked.
 // function checkTime(i) {
@@ -28,65 +97,3 @@ $( document ).ready(function() {
 //     }, 500);
 //   }
 //   startTime();
-
-// New way to set time...
-$(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
-  //Set up my time slots
-  var startHour = 9;
-  var endHour = 5;
-  function createPlanner() {
-    for (j = 9; j < 18; j++){
-      if (j >= 12){
-        var newRow = $("<div>", {id: j, "class": "row"});
-        var checkTime = j
-        console.log(checkTime);
-        if ( j > 12){
-          var PM = j - 12;
-        }
-        else {
-          var PM = 12;
-        }
-        var newTimeCol = $("<div>", {"class": "timeCol col-2"});
-        var newTextCol = $("<div>", {id: PM + " PM", "class": "TextCol col-9"});
-        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
-        newTimeCol.text(PM + " PM");
-        // console.log(checkTime);
-
-      }
-      else {
-        var AM = [j]
-        var checkTime = j;
-        console.log(checkTime);
-        var newRow = $("<div>", {id: j, "class": "row"});
-        var newTimeCol = $("<div>", {"class": "timeCol col-2"});
-        var newTextCol = $("<div>", {id: AM + " AM", "class": "TextCol col-9"});
-        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
-        newTimeCol.text(AM + " AM");
-        // console.log(checkTime)
-      }
-      var saveButton = $("<button>", {"class": "saveButton"});
-      $(".planner-body").append(newRow);
-      newRow.append(newTimeCol, newTextCol, newSaveCol);
-      var inputText = $('<textarea class="h-100 w-100" placeholder="So, what should I remember for you?">');
-      newTextCol.append(inputText);
-      newSaveCol.append(saveButton);
-      var HH = parseInt(moment().format('HH'), 10);
-      if (checkTime < HH) {
-        newTextCol.addClass("past");
-      }
-      else if (checkTime > HH) {
-        newTextCol.addClass("future");
-      }
-      else {
-        newTextCol.addClass("present");
-      }
-      console.log(HH);
-      console.log(checkTime);
-    }
-  }
-  createPlanner();
-  // function colorFields () {
-  //   var rowArray = 
-  // }
-
-});
