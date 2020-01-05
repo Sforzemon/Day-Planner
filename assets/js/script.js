92bf4fa6b542e7229749176c8814704fc71bd471
Sforzemon <sforzemon@gmail.com>
2020-01-04 23:47:05 -0500

    struggled with local storage, but I got it working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,48 @@
 $( document ).ready(function() {
 // New way to set time...
-$(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
-var update = function () {
   $(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
-};
-function setTime() {
-setInterval(update, 1000);
-}
-setTime()
-//Set up my time slots
-  var startHour = 9;
-  var endHour = 5;
+  function update() {
+    $(".current-time").text(moment().format('MMMM Do YYYY, h:mm:ss A'));
+  };
+  function setTime() {
+  setInterval(update, 1000);
+  };
+//Setting up my time slots
+//Create each row, 3 columns, and assign classes based onwhat the column should do and the time.
+//I set the function to check the time, then create PM or AM rows. 
+//It will then create the button
+// append the row and then the following columns
+// It assigns classes, and checks the row variable checkTime against moment() to apply past present or future classes.
   function createPlanner() {
     for (j = 9; j < 18; j++){
       if (j >= 12){
-        var newRow = $("<div>", {id: j, "class": "row"});
+        var newTextCol = $("<div>", {id: j, "class": "textCol col-9"});
         var checkTime = j
-        // console.log(checkTime);
         if ( j > 12){
           var PM = j - 12;
         }
         else {
           var PM = 12;
         }
+        var newRow = $("<div>", {id: PM + "-PM", "class": "row"});
         var newTimeCol = $("<div>", {"class": "timeCol col-2"});
-        var newTextCol = $("<div>", {id: PM + " PM", "class": "TextCol col-9"});
-        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
+        
+        var newSaveCol = $("<div>", {"class": "saveCol col-1"});
         newTimeCol.text(PM + " PM");
-        // console.log(checkTime);
-
       }
       else {
         var AM = [j]
         var checkTime = j;
-        // console.log(checkTime);
-        var newRow = $("<div>", {id: j, "class": "row"});
+        var newRow = $("<div>", {id: AM + "-AM", "class": "row"});
         var newTimeCol = $("<div>", {"class": "timeCol col-2"});
-        var newTextCol = $("<div>", {id: AM + " AM", "class": "TextCol col-9"});
-        var newSaveCol = $("<div>", {"class": "SaveCol col-1"});
+        var newTextCol = $("<div>", {id: j, "class": "textCol col-9"});
+        var newSaveCol = $("<div>", {"class": "saveCol col-1"});
         newTimeCol.text(AM + " AM");
-        // console.log(checkTime)
-      }
+      } 
       var saveButton = $("<button>", {"class": "saveButton"});
       $(".planner-body").append(newRow);
       newRow.append(newTimeCol, newTextCol, newSaveCol);
-      var inputText = $('<textarea class="h-100 w-100" placeholder="So, what should I remember for you?">');
+      var inputText = $('<textarea>').attr({"placeholder": "So, what should I remember for you?"});
       newTextCol.append(inputText);
       newSaveCol.append(saveButton);
       var HH = parseInt(moment().format('HH'), 10);
@@ -57,14 +55,68 @@ setTime()
       else {
         newTextCol.addClass("present");
       }
-      // console.log(HH);
-      // console.log(checkTime);
     }
   }
+
+//This next function will display the text in the same row as the button that was pressed to save.
+  var storedReminder = [];
+  if (localStorage.getItem('storedReminder') !== null) {
+    storedReminder = JSON.parse(localStorage.getItem('storedReminder'));
+  }
+  function showReminders() {
+     for (l = 0; l < storedReminder.length; l++) {
+       for (x = 9; x < 18; x++) {
+      if (x == storedReminder[l].key) {
+        $("#" + x).children().val(storedReminder[l].value)
+      }
+    }
+    }
+  }
+  // Making that save button work.
+  function saveReminder() {  
+    console.log($(this).parent().siblings(".textCol").attr("id"))
+    console.log($(this).parent().siblings(".textCol").children().val()) 
+    var savedReminder = {
+      key: $(this).parent().siblings(".textCol").attr("id"), 
+      value: $(this).parent().siblings(".textCol").children().val()
+    }
+    var keyCheck = 999;
+    // console.log($(this).parent().siblings(".textCol").attr("id"))
+    // console.log($(this).parent().siblings(".textCol").children().val())
+    if (storedReminder.length === 0) {
+      storedReminder.push(savedReminder);
+      console.log("did this work?")
+    } 
+    else {
+      for (var k = 0; k < storedReminder.length; k++) {
+        if (savedReminder.key === storedReminder[k].key) {
+          console.log("true")
+          keyCheck = k;
+          console.log(k + "this is k")
+          console.log(keyCheck)
+        } 
+      }
+      if (keyCheck === 999) {
+        storedReminder.push(savedReminder);
+        console.log(keyCheck + " if statement")
+      }
+      else {
+        storedReminder[keyCheck].key = savedReminder.key;
+        storedReminder[keyCheck].value = savedReminder.value;
+        console.log(keyCheck + " else statement")
+      } 
+    }
+    localStorage.setItem('storedReminder', JSON.stringify(storedReminder));
+  };
+  //Calling the clock that is displayed on the Jumbotron.
+  setTime();
+  //Calling the row and column div creation
   createPlanner();
+  //calling Local Storage saving on button click
+  $(document).on("click", ".saveButton", saveReminder);
+  //Calling Display Storage
+  showReminders();
 });
-
-
 
 
 
